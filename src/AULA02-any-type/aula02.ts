03e2a4ad42b45e9771d79d08195e3b474d090b95
Paulo silveira <paulo.silveira@usemobile.xyz>
2020-10-21 23:12:47 -0300

    chore: any type

---
+++ src/AULA02-any-type/aula02.ts
@@ -0,0 +1,9 @@
+// Utilize any apenas em ultimo caso
+
+function showMessage(msg: any) {
+  return msg;
+}
+
+console.log(showMessage('teste'));
+console.log(showMessage([1, 2, 3]));
+console.log(showMessage(35));
