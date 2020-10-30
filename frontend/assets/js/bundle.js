9ad916681328743463ae7d823868a21646794c3c
Paulo silveira <paulo.silveira@usemobile.xyz>
2020-10-29 23:46:27 -0300

    chore: add frontend-config

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/*!**************************************!*\
+  !*** ./src/AULA18-web-pack/index.ts ***!
+  \**************************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+console.log('Olá mundo');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
