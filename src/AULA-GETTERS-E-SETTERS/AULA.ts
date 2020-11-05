4232615cd945dfaa458f000f01589f8d31469131
Paulo silveira <paulo.silveira@usemobile.xyz>
2020-11-05 00:50:02 -0300

    chore: getter and setters

--- src/AULA-GETTERS-E-SETTERS/AULA.ts
+++ src/AULA-GETTERS-E-SETTERS/AULA.ts
@@ -21,10 +21,13 @@ export class Pessoa {
   }
 }
 
-const pessoa = new Pessoa('Paulo', 'Silveira', 21, '144.900.206-43');
+const pessoa = new Pessoa('Paulo', 'Silveira', 22, '144.900.206-43');
+const pessoa2 = new Pessoa('Teste', 'Oliveira', 22, '444.898.784-78');
 
 pessoa.cpf = '188.998.999.00'; // replace value using setter
 console.log(pessoa.cpf); // show value by getter
+
+console.log(pessoa2.cpf); // call person 2 with 1 setter
 
 // Maximum call stack size exceeded / stack overflow
 // resolved change this.cpf to this._cpf
