65ac618898be35a45e861c463806b5722a4e3cac
Paulo silveira <paulo.silveira@usemobile.xyz>
2020-11-16 20:52:11 -0300

    chore: add new class

--- src/AULA-GETTERS-E-SETTERS/AULA.ts
+++ src/AULA-GETTERS-E-SETTERS/AULA.ts
@@ -24,10 +24,31 @@ export class Pessoa {
 const pessoa = new Pessoa('Paulo', 'Silveira', 22, '144.900.206-43');
 const pessoa2 = new Pessoa('Teste', 'Oliveira', 22, '444.898.784-78');
 
-pessoa.cpf = '188.998.999.00'; // replace value using setter
+pessoa.cpf = '188.998.999-00'; // replace value using setter
 console.log(pessoa.cpf); // show value by getter
 
 console.log(pessoa2.cpf); // call person 2 with 1 setter
 
 // Maximum call stack size exceeded / stack overflow
 // resolved change this.cpf to this._cpf
+
+export class Carro {
+  constructor(
+    private brand: string,
+    private model: string,
+    private _board: number,
+  ) {
+    this.board = _board;
+  }
+
+  set board(value: number) {
+    this._board = value;
+  }
+
+  get board(): number {
+    return this._board;
+  }
+}
+
+const fiatToro = new Carro('Fiat', 'Toro', 5878);
+console.log(fiatToro.board);
